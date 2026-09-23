2185e96f6d32d239c3adc323348d2cbf639c947f
Umar Amroha <umarimam39@gmail.com>
2026-09-23 18:15:27 +0530

    fix: admin cookie path to / for API access

--- src/lib/adminSession.ts
+++ src/lib/adminSession.ts
@@ -19,6 +19,6 @@ export const adminSessionOptions: SessionOptions = {
     secure: process.env.NODE_ENV === "production",
     sameSite: "lax",
     maxAge: 60 * 60 * 24 * 7, // 7 days
-    path: "/admin", // <-- Sirf admin pages me bhejega
+    path: "/"
   },
 };
